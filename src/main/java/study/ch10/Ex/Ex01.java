260c0d0ea945a787af60dccb688f01e2944dd7cf
ginye28 <tsna1268@gmail.com>
2026-04-01 17:54:11 +0900

    0401 수업

--- src/main/java/study/ch10/Ex/Ex01.java
+++ src/main/java/study/ch10/Ex/Ex01.java
@@ -4,7 +4,7 @@ public class Ex01 {
     public static void main(String[] args) {
         int[] nums = {10, 20, 30, 40, 50};
         for (int i = 0; i < nums.length; i++) {
-            i
+//            i
         }
     }
 }
