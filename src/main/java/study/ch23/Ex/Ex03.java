3ad59ccfb2f42ad984c26d20a65e05f0650fc7b4
ginye <tsna1268@gmail.com>
2026-04-10 17:36:13 +0900

    0410 수업

--- src/main/java/study/ch23/Ex/Ex03.java
+++ src/main/java/study/ch23/Ex/Ex03.java
@@ -1,10 +1,11 @@
 package study.ch23.Ex;
 
+import java.util.List;
 import java.util.stream.Stream;
 
-public class Ex03 {
-    public static void main(String[] args) {
-        List<Integer> result = Stream.of(3, 7, 2, 9, 4, 11, 6)
-                .filter(n ->)
-    }
-}
+//public class Ex03 {
+//    public static void main(String[] args) {
+//        List<Integer> result = Stream.of(3, 7, 2, 9, 4, 11, 6)
+//                .filter(n ->)
+//    }
+//}
